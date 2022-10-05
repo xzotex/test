e734383c6a74da8beeb39559a3b04ce2a6eb12bb
Nikita <nekitsok@gmail.com>
2022-10-05 16:51:17 +0300

    first commit

---
+++ js1/script.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// const result = prompt("dsssdf", "");
+// console.log(typeof (result));
+
+const answers = [];
+
+// answers[0] = prompt('name?', '');
+// answers[1] = prompt('surname?', '');
+// answers[2] = prompt('age?', '');
+
+// 
+console.log(typeof (answers));
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Nikita";
+
+alert(`Привет, ${user}`);
+
+console.log('arr' + " - object");
+console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5 % 2);
